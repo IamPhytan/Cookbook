6d2d37ea4d704d6b19f019aeccca60511d560147
Damien LaRocque <phicoltan@gmail.com>
2021-06-02 10:42:05 -0400

    Add OpenCV installation script for RaspbPi

---
+++ python/opencv_snippets/opencv-contrib-python-installation.sh
@@ -0,0 +1,19 @@
+# OpenCV Contrib Python Dependencies Installation for Raspberry Pi
+
+sudo apt update && sudo apt upgrade
+sudo apt install -y build-essential cmake pkg-config
+sudo apt install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt install -y libxvidcore-dev libx264-dev
+sudo apt install -y libfontconfig1-dev libcairo2-dev
+sudo apt install -y libgtk2.0-dev
+sudo apt install -y libatlas-base-dev gfortran
+
+sudo apt install -y libgdk-pixbuf2.0-dev libpango1.0-dev
+sudo apt install -y libhdf5-dev libhdf5-serial-dev libhdf5-103
+sudo apt install -y libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
+
+sudo apt install -y libilmbase-dev libopenexr-dev
+
+
+sudo apt autoremove
