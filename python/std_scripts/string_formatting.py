4f0100f8ff82e168a00aa5ff8bc016fd91052336
Damien LaRocque <phicoltan@gmail.com>
2022-10-21 11:19:56 -0400

    String formatting

---
+++ python/std_scripts/string_formatting.py
@@ -0,0 +1,11 @@
+def main():
+    text = "TESTING"
+
+    print(f"{text:-<30}\n")
+    print(f"{text:_>30}\n")
+    print(f"{text:^30}\n")
+    print(f"{text:/^30}\n")
+
+
+if __name__ == "__main__":
+    main()
